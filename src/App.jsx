bf8c8a4ba27e2dee44a8837f66078a8481a03c79
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-01 13:39:43 -0300

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+
 import About from "./scenes/About"
 import Contact from "./scenes/Contact"
-import Header from "./scenes/Header"
+import Header from "./scenes/Hero"
+import Navbar from "./scenes/Navbar"
 import Projects from "./scenes/Projects"
 import Techs from "./scenes/Techs"
 
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home')
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  const [isTopOfPage, setIsTopOfPage] = useState(true)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true)
+      if(window.scrollY !== 0) setIsTopOfPage(false)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () =>   window.removeEventListener('scroll', handleScroll) 
+  }, [])
+
   return (
     <div className="app">
-      <Header />
-      <About />
-      <Techs />
-      <Projects />
-      <Contact />
+      <Navbar 
+         isTopOfPage={isTopOfPage}
+         selectedPage={selectedPage}
+         setSelectedPage ={setSelectedPage}
+      />
+      <div className="mt-24 ml-4">
+        <Header />
+        <About />
+        <Techs />
+        <Projects />
+        <Contact />
+      </div>
     </div>
   )
 }
