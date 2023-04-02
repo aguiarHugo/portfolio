09c261e7a19680c558b7d1ce0be74bb103e26169
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-02 19:57:49 -0300

    constans

---
+++ src/constants/index.js
@@ -0,0 +1,132 @@
+import github from '../assets/github.png'
+import dashboard from '../assets/dashboard.png'
+import gpt3 from '../assets/gpt3.png'
+import jsf from '../assets/jsf.png'
+import premiada from '../assets/premiada.png'
+import restaurant from '../assets/restaurant.png'
+import rocketnotes from '../assets/rocketnotes.png'
+
+export const projects = [
+  {
+    name: "Finance Dashboard",
+    description:
+      "MERN Finance Dashboard APP",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "MUI",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+      {
+        name: "express",
+        color: "green-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+    ],
+    image: dashboard,
+    source_code_link: "https://github.com/aguiarHugo/Finance-Dashboard-client",
+  },
+  {
+    name: "JSF",
+    description:
+      "Landing Page fully designed by me using ReactJs and Tailwind mainly",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: jsf,
+    source_code_link: "https://github.com/aguiarHugo/Jsf-contabilidade",
+  },
+  {
+    name: "Premiada",
+    description:
+    "Landing Page fully designed by me for Cachaça Premiada",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "framer motion",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: premiada,
+    source_code_link: "https://github.com/aguiarHugo/Premiada-LandingPage",
+  },
+  {
+    name: "Rocketnotes",
+    description:
+    "Web app developed during Rocketseat Explorer course",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "styled components",
+        color: "pink-text-gradient",
+      },
+      {
+        name: "nodejs",
+        color: "green-text-gradient",
+      },
+    ],
+    image: rocketnotes,
+    source_code_link: "https://github.com/aguiarHugo/Rocket-Notes",
+  },
+  {
+    name: "Gerícht",
+    description:
+    "Restaurant lading page developed during JS Mastery classes on youtube",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css-blocks-elements-modifiers",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: restaurant,
+    source_code_link: "https://github.com/aguiarHugo/restaurant-ui",
+  },
+  {
+    name: "GPT3",
+    description:
+    "GPT3 lading page developed during JS Mastery classes on youtube",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "css-blocks-elements-modifiers",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: gpt3,
+    source_code_link: "https://github.com/aguiarHugo/GPT3",
+  },
+];
