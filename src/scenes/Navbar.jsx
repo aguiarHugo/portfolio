bd690a66c8b62fc7d53797fab1aeb01f59571d44
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-03 11:27:03 -0300

    Styled

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -29,8 +29,14 @@ const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage }) => {
     <nav
       className={`${navbarBackground} z-40 w-full fixed top-0 py-6`}>
       <div className="flex items-center justify-between mx-auto w-5/6">
-        <FaReact size={32}/>
-
+        <FaReact 
+          className="cursor-pointer"
+          size={32}
+          onClick={() => {
+            setSelectedPage('')
+            window.scrollTo(0, 0,)
+        }}
+        />
         {/*DESKTOP */ }
         {isAboveSmallScreens ? (
           <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
