1eb3b910d373853f95db9dc564ee16b7f01d3abf
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-27 09:28:52 -0300

    Removed instagram from social medias

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,9 +1,9 @@
-import { AiFillGithub, AiFillLinkedin, AiFillInstagram } from 'react-icons/ai'
+import { AiFillGithub, AiFillLinkedin } from 'react-icons/ai'
 
 
 const SocialMedia = () => {
   return (
-    <div className="flex justify-center md:justify-start my-10 gap-7">
+    <div className="flex justify-center my-10 gap-7">
       <a
       className="hover:text-blue transition duration-500"
       href="https://www.linkedin.com/in/hugo-aguiar-a25839245/"
@@ -20,15 +20,6 @@ const SocialMedia = () => {
       rel="noreferrer"
       >
         <AiFillGithub size={40}/>
-      </a>
-
-      <a
-      className="hover:text-blue transition duration-500"
-      href="https://www.instagram.com/h4guiar/"
-      target="_blank"
-      rel="noreferrer"
-      >
-        <AiFillInstagram size={40}/>
       </a>
     </div>
   )
