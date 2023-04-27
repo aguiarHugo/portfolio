d13c7767981aa24449c6804a913eb706fde68efa
aguiarHugo <aguiarhugofx@gmail.com>
2023-04-27 10:08:15 -0300

    Removed HTML & CSS

--- src/components/TechsCard.jsx
+++ src/components/TechsCard.jsx
@@ -1,6 +1,7 @@
-import { DiReact, DiNodejsSmall, DiHtml5 } from 'react-icons/di';
+import { DiReact, DiNodejsSmall } from 'react-icons/di';
 import { SiJavascript, SiTailwindcss, SiStyledcomponents, SiMui } from 'react-icons/si';
-import { FaCss3Alt, FaBootstrap } from 'react-icons/fa'
+import { FaBootstrap } from 'react-icons/fa'
+import { TbBrandNextjs } from 'react-icons/tb'
 
 import React from 'react'
 
@@ -8,16 +9,13 @@ const TechsCard = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
         <div className="w-28 h-28 cursor-pointer bg-purple-400 flex items-center justify-center rounded-lg">
-          <DiHtml5 size={72} />
-        </div>
-        <div className="w-28 h-28 cursor-pointer bg-purple-400 flex items-center justify-center rounded-lg">
-          <FaCss3Alt size={72} />
-        </div>
-        <div className="w-28 h-28 cursor-pointer bg-purple-400 flex items-center justify-center rounded-lg">
           <SiJavascript size={72} />
         </div>
         <div className="w-28 h-28 cursor-pointer bg-purple-400 flex items-center justify-center rounded-lg">
           <DiReact size={72} />
+        </div>
+        <div className="w-28 h-28 cursor-pointer bg-purple-400 flex items-center justify-center rounded-lg">
+          <TbBrandNextjs size={72} />
         </div>
         <div className="w-28 h-28 cursor-pointer bg-purple-400 flex items-center justify-center rounded-lg">
           <SiTailwindcss size={72} />
